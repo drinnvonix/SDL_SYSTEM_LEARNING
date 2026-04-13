066ec5de792d929274227fe75ca8647d550e02ab
Dhrumil Trivedi <dr.innvonix@gmail.com>
2026-04-13 10:03:48 +0530

    Initial commit

---
+++ src/main/java/com/example/sdl_system_learning/SdlSystemLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdl_system_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdlSystemLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdlSystemLearningApplication.class, args);
+    }
+
+}
